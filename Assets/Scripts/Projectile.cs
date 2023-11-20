a613a6d7e23ed5ca35c2b673a6431c5fcefe4f5a
OnurKalkan <onurkalkanv84@hotmail.com>
2023-11-21 02:52:56 +0300

    Some fixes and paint

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,21 +5,53 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public int speed = 1000;
+    public bool auto, semi_auto;
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0) && auto && transform.childCount == 4)
         {
             GameObject proj = transform.Find("Projectile").gameObject;
             proj.transform.parent = null;
+            proj.GetComponent<MeshRenderer>().enabled = true;
             proj.GetComponent<Rigidbody>().useGravity = true;
             proj.GetComponent<Rigidbody>().isKinematic = false;
             proj.GetComponent<Rigidbody>().AddForce(transform.Find("Sample Projectile").transform.forward * speed);
-            Invoke(nameof(NewProjectile), 0.1f);
+            StartCoroutine(BallDestroyer(proj));
+            Invoke(nameof(NewProjectile), 0.05f);
+        }
+        else if (Input.GetMouseButtonDown(0) && semi_auto)
+        {
+            GameObject proj = transform.Find("Projectile").gameObject;
+            proj.transform.parent = null;
+            proj.GetComponent<MeshRenderer>().enabled = true;
+            proj.GetComponent<Rigidbody>().useGravity = true;
+            proj.GetComponent<Rigidbody>().isKinematic = false;
+            proj.GetComponent<Rigidbody>().AddForce(transform.Find("Sample Projectile").transform.forward * speed);
+            StartCoroutine(BallDestroyer(proj));
+            Invoke(nameof(NewProjectile), 0.05f);
+        }
+        if(auto && Input.GetKeyDown(KeyCode.Q))
+        {
+            auto = false;
+            semi_auto = true;
+            print("semi-auto");
+        }
+        else if (semi_auto && Input.GetKeyDown(KeyCode.Q))
+        {
+            semi_auto = false;
+            auto = true;
+            print("auto");
         }
     }
     void NewProjectile()
     {
         GetComponent<ProjectileSpawner>().CreateBall();
+    }
+
+    IEnumerator BallDestroyer(GameObject proj)
+    {
+        yield return new WaitForSeconds(3);
+            Destroy(proj);
     }
 
 }
--- Assets/Scripts/ProjectileSpawner.cs
+++ Assets/Scripts/ProjectileSpawner.cs
@@ -9,17 +9,13 @@ public class ProjectileSpawner : MonoBehaviour
     public void CreateBall()
     {
         GameObject ball = Instantiate(ballPrefab, ballPrefab.transform.position, Quaternion.identity);
+        ball.GetComponent<MeshRenderer>().enabled = false;
         ball.gameObject.SetActive(true);
         ball.name = "Projectile";
         ball.transform.parent = this.transform;
         ball.GetComponent<Renderer>().material.color = new Color (Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f),
-            Random.Range(0.0f, 1.0f));
-        StartCoroutine(BallDestroyer(ball));
+            Random.Range(0.0f, 1.0f));        
     }
 
-    IEnumerator BallDestroyer(GameObject proj)
-    {
-        yield return new WaitForSeconds(3);
-        Destroy(proj);
-    }
+    
 }
